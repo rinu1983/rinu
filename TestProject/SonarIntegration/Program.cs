75cd6aa8c29f0f97a10ea92f9fd38640f68bea83
Rinu Rajan <rinu_rajan@baxter.com>
2024-05-22 12:16:05 +0530

    Initial version

---
+++ TestProject/SonarIntegration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SonarIntegration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
